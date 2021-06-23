a96211fe5d28b6a0cc4906962aa267321fe04047
MichaelShen <michael830725@gmail.com>
2021-06-24 02:26:32 +0800

    Remove Linked List elements

---
+++ Algorithms/LinkedList/203. Remove Linked List Elements.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+## By LinkedList 架構
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return None
+
+        # move head until its value is not val
+        while head and head.val == val:
+            head = head.next
+
+        else:
+            previous_node = None
+            current_node = head
+
+            while current_node:
+
+
+                # remove all consequitive vals
+                while current_node and current_node.val == val:
+                    previous_node.next = current_node.next
+                    current_node = current_node.next
+
+                # or move pointers
+                else:
+
+                    if current_node:
+                        previous_node = current_node
+                        current_node = current_node.next
+        return head
+
