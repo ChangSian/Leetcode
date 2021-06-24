9fdb9f378c46a53f1e95ba3412956be3d4b72171
MichaelShen <michael830725@gmail.com>
2021-06-25 02:06:52 +0800

    設置一個虛擬頭節點在進行刪除操作

--- Algorithms/LinkedList/203. Remove Linked List Elements.py
+++ Algorithms/LinkedList/203. Remove Linked List Elements.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 
-## By LinkedList 架構
+## 直接使用原来的鏈表来進行刪除操作
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
         if not head:
@@ -33,4 +33,31 @@ class Solution:
                         previous_node = current_node
                         current_node = current_node.next
         return head
+
+## 設置一個虛擬頭節點在進行刪除操作
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        
+        DummyNode = ListNode()
+        DummyNode.next = head
+
+        previous_node = DummyNode
+        current_node = DummyNode.next
+
+        while current_node:
+            
+            # remove all consequitive vals
+            while current_node and current_node.val == val:
+                
+                previous_node.next = current_node.next
+                current_node = current_node.next
+
+             # or move pointers
+            else:
+                if current_node:
+                    previous_node = current_node
+                    current_node = current_node.next
+                        
+        return DummyNode.next
+
 
